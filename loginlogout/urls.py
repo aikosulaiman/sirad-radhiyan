130ea5283b3f7c3cfee3dc8db86b0545c3ad7c1e
Aiko Sulaiman <aiko.sulaiman@ui.ac.id>
2023-03-11 12:54:31 +0700

    test

--- loginlogout/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from loginlogout.views import *
-
-urlpatterns = [
-    path('', index, name='Index'),
-]
--- propensid02/settings.py
+++ propensid02/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "sirad",
-    "loginlogout",
 ]
 
 MIDDLEWARE = [
