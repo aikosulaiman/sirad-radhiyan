ffc64940b589a10168340be95827a98aa7d72461
Rayyan Azka Ihsanuddin <rayyan.azka@ui.ac.id>
2023-03-16 16:22:47 +0700

    create html files

--- sirad/urls.py
+++ sirad/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='Index'),
+    path('customer-registration/', views.customer_registration, name="Customer Registration"),
+    path('read-profile/', views.read_profile, name="Read Profile")
 ]
--- sirad/views.py
+++ sirad/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     return render(request, 'base.html')
+
+def customer_registration(request):
+    return render(request, 'customer-registration.html')
+
+def read_profile(request):
+    return render(request, 'read-profile.html')
