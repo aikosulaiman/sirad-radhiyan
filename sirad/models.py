c67d69edacf2f9765c1a90888b1297fe917c4209
Rayyan Azka Ihsanuddin <rayyan.azka@ui.ac.id>
2023-03-18 15:56:11 +0700

    revise html

--- sirad/models.py
+++ sirad/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-# class User(models.Model):
+
+# class User(AbstractUser):
+#     pass
 #     id = models.UUIDField
 #     nama = models.CharField(max_length=100)
 #     role = models.CharField(max_length=100)
