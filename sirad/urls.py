dfcb28587632d95132b2c5cbab8c7686e2929b15
Rayyan Azka Ihsanuddin <rayyan.azka@ui.ac.id>
2023-03-20 15:46:18 +0700

    mengganti url pada app sirad

--- sirad/urls.py
+++ sirad/urls.py
@@ -2,8 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('customer-registration/', views.customer_registration, name="Customer Registration"),
-    path('', views.index, name='Index'),
-    path('form-pendaftaran-hewan/', views.form_pendaftaran_hewan, name="Form Pendaftaran Hewan"),
-    path('payment-form/', views.payment_form, name="Payment Form"),
+    path('', views.index, name='Index')
 ]
