1b36299ebcba02e1c4abee5e0032f96c73340673
fatimatriputra7 <fatimatriputra7@gmail.com>
2023-03-20 13:58:10 +0700

    minor fix

--- propensid02/urls.py
+++ propensid02/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('sirad.urls')),
+    path('', include('loginlogout.urls')),
     path('profile/', include('update_profile.urls')),
 ]
