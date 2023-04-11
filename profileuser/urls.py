d89f9acd074ddf7e1c3487ccb0ddcea44185ac9b
RayyanAzka <rayyan.azka.i@gmail.com>
2023-04-11 15:58:34 +0000

    Rayyan readprofile

--- profileuser/urls.py
+++ profileuser/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, update_profile, update_profile_handler, form_pendaftaran_hewan, payment_form, update_hewan, update_hewan_handler
+from .views import index, update_profile, update_profile_handler, update_hewan, update_hewan_handler, read_profile, submit_vip_validation, tambah_hewan, tambah_hewan_handler, delete_hewan
 
 urlpatterns = [
     path('', index, name='Index'),
@@ -8,5 +8,10 @@ urlpatterns = [
     path('form-pendaftaran-hewan/', form_pendaftaran_hewan, name="form_pendaftaran_hewan"),
     path('payment-form/', payment_form, name="Payment Form"),
     path('update-hewan/<int:user_id>', update_hewan, name='update_hewan'),
-    path('update-hewan-handler/<int:user_id>', update_hewan_handler, name='update_hewan_handler')
+    path('update-hewan-handler/<int:user_id>', update_hewan_handler, name='update_hewan_handler'),
+    path('read-profile/<str:username>', read_profile, name='read_profile'),
+    path('submit-vip-validation/<uuid:user_id>', submit_vip_validation, name='submit_vip_validation'),
+    path('tambah-hewan/<uuid:user_id>', tambah_hewan, name='tambah_hewan'),
+    path('tambah-hewan-handler/<uuid:user_id>', tambah_hewan_handler, name='tambah_hewan_handler'),
+    path('delete-hewan/<int:hewan_id>', delete_hewan, name='delete_hewan'),
 ]
--- profileuser/views.py
+++ profileuser/views.py
@@ -1,14 +1,108 @@
 import uuid
 from django.http.response import HttpResponseRedirect
-from django.shortcuts import render, redirect
 from django.conf import settings
-# from ..profileuser.forms import UserForm
-from .models import User, Customer, Hewan
 from django.db import IntegrityError, connection
-from .forms import FormPendaftaranHewan
+from django.db import connection
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.shortcuts import redirect
+from django.http import HttpResponse
+import io
+from PIL import Image
+from supabase.client import Client
+
+SUPABASE_URL = settings.SUPABASE_URL
+SUPABASE_KEY = settings.SUPABASE_KEY
 
 def index(request):
     return HttpResponseRedirect("/")
+
+def is_authenticated(request):
+    try:
+        request.session['Username']
+        return True
+    except:
+        return False
+
+def read_profile(request, username):
+    if is_authenticated(request):
+        cursor = connection.cursor()
+        cursor.execute("SET SEARCH_PATH TO PUBLIC;")
+
+        # Mencari user
+        cursor.execute("""
+        SELECT *
+        FROM user_user
+        WHERE username = '{0}' ;
+        """.format(username))
+        user = cursor.fetchall()
+            
+        response = {
+                'user_id':user[0][0],
+                'username':username,
+                'user_first_name':user[0][2],
+                'user_last_name':user[0][3],
+                'user_role':user[0][4],
+                'user_email':user[0][5],
+                'user_no_telepon':user[0][6],
+                'user':user}
+
+        if response['user_role'] == 'Customer':
+            cursor.execute("""
+            SELECT *
+            FROM user_customer
+            WHERE user_ptr_id = '{0}' ;
+            """.format(response['user_id']))
+            user = cursor.fetchall()
+
+            response['user_is_vip'] = user[0][1]
+
+            cursor.execute("""
+            SELECT *
+            FROM user_hewan
+            WHERE pemilik_id = '{0}' ;
+            """.format(response['user_id']))
+            list_hewan = cursor.fetchall()
+            print(list_hewan)
+
+            response['list_hewan'] = list_hewan
+
+            cursor.close()
+            return render(request, 'read_profile_customer.html', response)
+        elif response['user_role'] == 'Dokter':
+            cursor.execute("""
+            SELECT *
+            FROM user_dokter
+            WHERE user_ptr_id = '{0}' ;
+            """.format(response['user_id']))
+            user = cursor.fetchall()
+
+            response['user_tarif'] = user[0][1]
+            cursor.close()
+            return render(request, 'read_profile_dokter.html', response)
+        else:
+            cursor.close()
+            return render(request, 'read_profile.html', response)
+    else:
+        return HttpResponseRedirect("/login")
+
+def submit_vip_validation(request, user_id):
+    cursor = connection.cursor()
+    cursor.execute("SET SEARCH_PATH TO PUBLIC;")
+
+    # Mencari user
+    cursor.execute("""
+    SELECT *
+    FROM user_user
+    WHERE id = '{0}' ;
+    """.format(user_id))
+    user = cursor.fetchall()
+        
+    response = {
+            'user_id':user_id,
+            'user':user,}
+    cursor.close()
+    return render(request, 'submit_vip_validation.html', response)
 
 def update_profile(request, user_id):
     cursor = connection.cursor()
@@ -68,31 +162,72 @@ def update_profile_handler(request, user_id):
     cursor.close()
     return HttpResponseRedirect('/profile')
 
-def form_pendaftaran_hewan(request) :
+# def tambah_hewan(request, user_id):
+#     form = HewanForm()
+#     if request.method == 'POST':
+#         form = HewanForm(request.POST or None)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect('/')
+#         else:
+#             print(form.errors)
+#             # form.add_error(None, "Username or email already exists, please choose another one!")
+#     response = {
+#         'form': form,
+#         'user_id':user_id
+#         }
+#     return render(request, 'tambah_hewan.html', response)
+
+def tambah_hewan(request, user_id):
     cursor = connection.cursor()
-    if request.method == 'POST':
-        form = FormPendaftaranHewan(request.POST)
-        if form.is_valid():
-            hewan_id = uuid.uuid4()
-            nama = form.cleaned_data['nama']
-            jenis = form.cleaned_data['jenis']
-            umur = form.cleaned_data['umur']
-            note = form.cleaned_data['note']
-            cursor.execute("SET search_path TO public")
-            cursor.execute('INSERT INTO profileuser_hewan VALUES (%s, %s, %s, %s, %s)', [hewan_id, nama, jenis, umur, note])
-            return redirect('/profile')
-    else:
-        form = FormPendaftaranHewan()
-    return render(request, 'form_pendaftaran_hewan.html', {'form': form})
+    cursor.execute("SET SEARCH_PATH TO PUBLIC;")
 
-# def delete_hewan(request, id):
-#     hewan_by_id = Hewan.objects.get(id=id)
-#     hewan_by_id.delete()
-#     return HttpResponseRedirect('/profile')
+    # Mencari user
+    cursor.execute("""
+    SELECT *
+    FROM user_user
+    WHERE id = '{0}' ;
+    """.format(user_id))
+    user = cursor.fetchall()
+        
+    response = {
+            'user_id':user_id,
+            'user':user,}
+    cursor.close()
+    return render(request, 'tambah_hewan.html', response)
 
-def payment_form(request):
-    context = {}
-    return render(request, 'payment_form.html', context)
+def tambah_hewan_handler(request, user_id):
+    cursor = connection.cursor()
+    cursor.execute("SET SEARCH_PATH TO PUBLIC;")
+
+    # get data dari form
+    nama = request.POST.get('nama')
+    jenis = request.POST.get('jenis')
+    umur = request.POST.get('umur')
+    note = request.POST.get('note')
+
+    try:
+        # update
+        cursor.execute('''
+        INSERT INTO user_hewan (nama, jenis, umur, note, pemilik_id)
+        VALUES (%s, %s, %s, %s, %s);
+        ''', (nama, jenis, umur, note, user_id))
+        success_message = 'Hewan Anda berhasil didaftarkan!'
+        return render(request, 'tambah_hewan_success.html', {'success_message': success_message})
+    except IntegrityError as e:
+        # If the field is not unique, return an error message
+        error_message = 'Hewan Anda gagal ditambahkan.'
+        cursor.execute("""
+        SELECT *
+        FROM user_user
+        WHERE id = '{0}' ;
+        """.format(user_id))
+        user = cursor.fetchall()
+        response = {
+            'error_message': e.__cause__,
+            'user':user,
+            'user_id': user_id}
+        return render(request, 'tambah_hewan.html', response)
 
 def update_hewan(request, user_id):
     cursor = connection.cursor()
@@ -149,3 +284,17 @@ def update_hewan_handler(request, user_id):
         
     cursor.close()
     return HttpResponseRedirect('/profile')
+
+def delete_hewan(request, hewan_id):
+    cursor = connection.cursor()
+    cursor.execute("SET SEARCH_PATH TO PUBLIC;")
+
+    # Mencari user
+    cursor.execute("""
+    DELETE
+    FROM user_hewan
+    WHERE hewan_id = '{0}' ;
+    """.format(hewan_id))
+    cursor.close()
+    success_message = 'Hewan ini berhasil dihapus!'
+    return render(request, 'delete_success.html', {'success_message': success_message})
