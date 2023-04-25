5964d864ab592e48328a404b8ab3dd7f789996c7
Amanda Putri <amandaputrik@192.168.0.169>
2023-04-25 16:21:43 +0700

    update event tanpa checkbox vip

--- events/urls.py
+++ events/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import create_event, list_event
+from .views import create_event, list_event, update_event, update_event_handler
 
 urlpatterns = [
     path('', list_event, name='list_event'),
     path('create-event', create_event, name='create_event'),
+    path('update-event/<uuid:event_id>', update_event, name='update_event'),
+    path('update-event-handler/<uuid:event_id>', update_event_handler, name='update_event_handler'),
 ]
