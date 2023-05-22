24bfdce8c50b33cae91590791de5eb0030db20f1
Rayyan Azka Ihsanuddin <rayyan.azka@ui.ac.id>
2023-05-22 10:14:18 +0700

    merge bar charts into a single page

--- statistik/urls.py
+++ statistik/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('statistik-dokter', statistik_dokter, name='statistik_dokter'),
-    path('statistik-grooming', statistik_grooming, name='statistik_grooming'),
+    path('statistik', statistik, name='statistik'),
+    # path('statistik-dokter', statistik_dokter, name='statistik_dokter'),
+    # path('statistik-grooming', statistik_grooming, name='statistik_grooming'),
 ]
