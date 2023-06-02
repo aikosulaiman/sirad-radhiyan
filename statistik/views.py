037ca4f3270b89e7354a6de6802f2b846328d87e
Rayyan Azka Ihsanuddin <rayyan.azka@ui.ac.id>
2023-06-02 14:43:23 +0700

    javascript

--- statistik/views.py
+++ statistik/views.py
@@ -257,54 +257,23 @@ def statistik_grooming(request):
             print(i[1])
 
         # Format the data for the chart
-        month_names = []
+        master_data = []
         appointment_counts = []
         for appointment in list_master_data:
             month = appointment[0]
             count = appointment[1]
-            month_names.append(month)
+            master_data.append(month)
             appointment_counts.append(count)
-        print(month_names)
+        print(master_data)
         print(appointment_counts)
 
-        labels_json = json.dumps(month_names, default=str)
+        labels_json = json.dumps(master_data, default=str)
         data_json = json.dumps(appointment_counts)
 
         print(labels_json)
 
-        nama_bulan = []
-        for label in labels_json:
-            if label == '1':
-                nama_bulan.append("Januari")
-            elif label == '2':
-                nama_bulan.append("Februari")
-            elif label == '3':
-                nama_bulan.append("Maret")
-            elif label == '4':
-                nama_bulan.append("April")
-            elif label == '5':
-                nama_bulan.append("Mei")
-            elif label == '6':
-                nama_bulan.append("Juni")
-            elif label == '7':
-                nama_bulan.append("Juli")
-            elif label == '8':
-                nama_bulan.append("Agustus")
-            elif label == '9':
-                nama_bulan.append("September")
-            elif label == '10':
-                nama_bulan.append("Oktober")
-            elif label == '11':
-                nama_bulan.append("November")
-            elif label == '12':
-                nama_bulan.append("Desember")
-
-        print(nama_bulan)
-            
-        labels_json_namabulan = json.dumps(nama_bulan, default=str)
-
         context = {
-        'month_names': labels_json_namabulan,
+        'master_data': labels_json,
         'appointment_counts': data_json,
     }
 
